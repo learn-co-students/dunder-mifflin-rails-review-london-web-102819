7c587f86c09e7313111baab251bbebb0bf92f094
andrew-lewell <andrew.lewell@gmail.com>
2019-11-27 16:56:45 +0000

    Done.

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,27 @@
 class DogsController < ApplicationController
+
+    def index
+        @dogs = Dog.all 
+    end 
+
+    def show 
+        @dog = Dog.find(params[:id])
+    end 
+
+    # def new 
+    #     @dog = Dog.new
+    # end 
+
+    # def edit
+    #     @dog = Dog.find(params[:id])
+    # end 
+
+    # def create
+
+    # end 
+
+    # def update
+    
+    # end 
+
 end
