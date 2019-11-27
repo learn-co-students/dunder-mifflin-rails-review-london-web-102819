6343aa27173aecae69273e5bf98453178f3e6e06
Qing <chingwang.qw@gmail.com>
2019-11-27 14:31:00 +0000

    Done.

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,39 @@
 class EmployeesController < ApplicationController
+  def index
+    @employees = Employee.all
+  end
+
+  def new
+    @employee = Employee.new
+  end
+
+  def show
+    @employee = Employee.find(params[:id])
+  end
+
+  def create
+    @employee = Employee.new(employee_params)
+    @employee.save
+
+    redirect_to employee_path(@employee)
+  end
+
+  def edit
+    @employee = Employee.find(params[:id])
+  end
+
+  def update
+    @employee = Employee.find(params[:id])
+    if @employee.update(employee_params)
+      redirect_to employee_path(@employee)
+    else
+      render new_employee_path
+    end
+  end
+
+  private
+
+  def employee_params
+    params.require(:employee).permit(:first_name, :last_name, :alias, :title, :office, :img_url, :dog_id)
+  end
 end
--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,2 +1,7 @@
 class Dog < ApplicationRecord
+  has_many :employees
+
+  # def employee
+  #   self.employee.map { |e| e.name }
+  # end
 end
