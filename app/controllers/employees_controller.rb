ffe33c44b8ac62f8cf2c614a6e44e9268b568a6b
github username <github email address>
2019-11-27 15:00:40 +0000

    Done.

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,44 @@
 class EmployeesController < ApplicationController
+
+    def index
+        @employees = Employee.all
+    end
+
+    def show
+        @employee = Employee.find(params[:id])
+    end
+
+    def new
+        @employee = Employee.new
+    end
+
+    def create
+        @employee = Employee.new(e_params)
+
+        if @employee.save
+            redirect_to employee_path(@employee)
+        else
+            render :new
+        end
+    end
+
+    def edit
+        @employee = Employee.find(params[:id])
+    end
+
+    def update
+        @employee = Employee.find(params[:id])
+
+        if @employee.update(e_params)
+            redirect_to employee_path(@employee)
+        else
+            render :edit
+        end
+    end
+
+    private
+
+    def e_params
+        params.require(:employee).permit(:first_name, :last_name, :alias, :title, :img_url, :office, :dog_id)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :dogs
-  resources :employees
+  get '/dogs/sorted', to: "dogs#sort", as: :dogs_sort
+  resources :dogs, only: [:show, :index]
+  resources :employees, except: :destroy
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  
 end
